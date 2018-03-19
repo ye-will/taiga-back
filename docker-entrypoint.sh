d971a9252be8739dfa376316ed6f8559b47b4a93
yewei <yewei@nibirutech.com>
2018-03-19 23:05:19 +0800

    add docker support

---
+++ docker-entrypoint.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+taiga::init() {
+  python manage.py migrate --noinput
+  python manage.py loaddata initial_user
+  python manage.py loaddata initial_project_templates
+  python manage.py compilemessages
+  python manage.py collectstatic --noinput
+}
+
+taiga::start() {
+  chown -R taiga /taiga
+  su taiga << EOF
+    gunicorn -w 3 -t 60 --pythonpath=. -b 0.0.0.0:8000 taiga.wsgi
+EOF
+}
+
+main() {
+  case "X$1" in
+    "Xinit" )
+      taiga::init
+      ;;
+    "X" )
+      taiga::start
+      ;;
+    * )
+      echo "hello from taiga-back"
+      ;;
+  esac
+}
